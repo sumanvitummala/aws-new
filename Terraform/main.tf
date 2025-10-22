fd3dfd05ae232caf2ff4e8df21c24bc9c98e2138
Sai Sumanvi Tummala <sumanvi.tummala2004@gmail.com>
2025-10-22 14:20:46 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -137,14 +137,4 @@ resource "aws_instance" "web" {
   }
 }
 
-# ------------------------------
-# Outputs
-# ------------------------------
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "ec2_public_dns" {
-  value = aws_instance.web.public_dns
-}
 
