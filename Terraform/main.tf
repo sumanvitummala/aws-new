880a7e9ec62a037279b0c57a94ee06160bdad61c
Sai Sumanvi Tummala <sumanvi.tummala2004@gmail.com>
2025-11-06 14:44:51 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -152,7 +152,9 @@ resource "aws_instance" "web" {
   key_name             = var.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   security_groups      = [aws_security_group.web_sg.name]
-
+  lifecycle {
+    create_before_destroy = false
+  }
   user_data = <<-EOF
 #!/bin/bash
 yum update -y
